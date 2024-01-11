23ab9ff1f873d653d9e17d8e0a452ffb0ab635a6
Vlado46 <110978223+Vlado46@users.noreply.github.com>
2024-01-11 21:59:25 +0100

    Initial

---
+++ screens/AllExpenses.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const AllExpenses = () => {
+  return <Text>AllExpenses scr</Text>;
+};
+
+export default AllExpenses;
---
+++ screens/ManageExpense.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const ManageExpense = () => {
+  return <Text>ManageExpense scr</Text>;
+};
+
+export default ManageExpense;
